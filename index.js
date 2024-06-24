b911d51a803debac22a875325ea44b581881b25b
Essi Purhonen <pessiapps@gmail.com>
2024-06-24 15:57:19 +0300

    puhelinluettelo backend 3.9

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
-const morgan = require('morgan')
 const app = express()
+const morgan = require('morgan')
+const cors = require('cors')
 const api = '/api/persons'
+
 let persons =  [
   {
     name: "Arto Hellas",
@@ -24,8 +26,9 @@ let persons =  [
     id: 4
   }
 ]
-morgan.token('body', (req) => JSON.stringify(req.body))
 
+morgan.token('body', (req) => JSON.stringify(req.body))
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body')) // tiny with token body
 
@@ -71,7 +74,7 @@ app.post(`${api}`, (req, res) => {
     res.json(newPerson)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
